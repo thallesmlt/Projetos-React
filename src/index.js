5075ef8737c4cfb0aa3b795d7745ecdf92418122
Thalles <thallescaltabiano@gmail.com>
2021-04-08 18:38:17 -0300

    Calculadora usando react

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
+
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>Calculator</h1>
+      <Calculator/>
+    </div>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//registerServiceWorker();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
